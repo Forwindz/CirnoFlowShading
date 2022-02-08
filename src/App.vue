150d4317d5056fb59916bc6ddaf8d813cfc5aaeb
Forwindz <dfxxlyc@126.com>
2022-02-08 23:43:00 +0100

    Add 3D scene

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
-  <Rete/>
+  <Scene3D style = "width:20%;height:20vh;position:fixed;padding:10px"></Scene3D>
+  <Rete style = "width:100%;height:80vh"></Rete>
 </template>
 
 <script>
 import Rete from './components/Rete.vue'
+import Scene3D from './components/Scene3D.vue'
 export default {
   name: 'App',
   components: {
-    Rete
+    Rete,
+    Scene3D
   }
 }
 </script>
---
+++ src/components/Scene3D.vue
@@ -0,0 +1,105 @@
+<template>
+    <div ref="canvas" id="canvas"></div>
+</template>
+
+<script>
+// import { Clock, PerspectiveCamera, Scene, WebGLRenderer } from 'three'
+import * as THREE from 'three'
+import TrackballControls from 'three-trackballcontrols'
+/*
+import {
+    BloomEffect,
+    EffectComposer,
+    GlitchPass,
+    EffectPass,
+    RenderPass
+} from 'postprocessing'*/
+
+export default {
+    name: 'Scene3D',
+    data: function() {
+        return {}
+    },
+    created: function() {
+        
+        
+        
+    },
+    mounted: function() {
+        const scene = new THREE.Scene()
+        // const composer = new THREE.EffectComposer(new WebGLRenderer())
+        // const effectPass = new THREE.EffectPass(camera, new BloomEffect())
+        const camera = new THREE.PerspectiveCamera(
+            75,
+            this.$refs.canvas.clientWidth / this.$refs.canvas.clientHeight,
+            0.1,
+            1000
+        )
+        const renderer = new THREE.WebGLRenderer({ antialias: true })
+        const light = new THREE.DirectionalLight('hsl(0, 100%, 100%)')
+        const geometry = new THREE.BoxGeometry(1, 1, 1)
+        const material = new THREE.MeshStandardMaterial({
+            side: THREE.FrontSide,
+            color: 'hsl(0, 100%, 50%)',
+            wireframe: false
+        })
+        const cube = new THREE.Mesh(geometry, material)
+        const axes = new THREE.AxesHelper(5)
+
+        this.eles = {
+            scene: scene,
+            camera: camera,
+            controls: [],
+            renderer: renderer,
+            light: light,
+            cube: cube,
+            axes: axes,
+            speed: 0.01
+        }
+        
+        this.$refs.canvas.appendChild(this.eles.renderer.domElement)
+        this.eles.scene.add(this.eles.camera)
+        this.eles.scene.add(this.eles.light)
+        this.eles.scene.add(this.eles.cube)
+        this.eles.scene.add(this.eles.axes)
+        this.eles.light.position.set(0, 0, 10)
+        this.eles.camera.position.z = 15
+        this.eles.scene.background = new THREE.Color('rgb(233,233,233)')
+        console.log(this.$refs.canvas.clientWidth, this.$refs.canvas.clientHeight)
+        this.eles.renderer.setSize(this.$refs.canvas.clientWidth, this.$refs.canvas.clientHeight)
+        this.eles.controls = new TrackballControls(this.eles.camera,this.$el)
+        this.eles.controls.rotateSpeed = 1.0
+        this.eles.controls.zoomSpeed = 5
+        this.eles.controls.panSpeed = 0.8
+        this.eles.controls.noZoom = false
+        this.eles.controls.noPan = false
+        this.eles.controls.staticMoving = true
+        this.eles.controls.dynamicDampingFactor = 0.3
+        this.animate()
+        
+    },
+    methods: {
+        animate: function() {
+            requestAnimationFrame(this.animate)
+            this.eles.renderer.render(this.eles.scene, this.eles.camera)
+            this.eles.cube.rotation.y += this.eles.speed
+            this.eles.controls.update()
+        }
+    },
+    computed: {
+        rotate: function() {
+            if (this.eles.speed === '') {
+                return 0
+            } else {
+                return this.eles.speed
+            }
+        }
+    }
+}
+</script>
+<style>
+canvas {
+    width: 100vw;
+    height: 100vh;
+}
+</style>
